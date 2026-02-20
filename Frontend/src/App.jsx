4d73c89cee8d7d9f7b6e09d75f1aff47aa581971
Mustafa Ali <mustafaramakda61@gmail.com>
2026-02-20 20:42:02 +0300

    new context created and wrapped throughout the app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,19 @@
-import {
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
+import { AuthProvider } from "./components/auth/auth.context";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/profile" element={<Profile />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </AuthProvider>
   );
 }
 
---
+++ Frontend/src/components/auth/auth.context.js
@@ -0,0 +1,41 @@
+import { createContext, useState, useEffect } from "react";
+import { login, register, getMe } from "./services/auth.api";
+
+export const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleLogin = async (email, password) => {
+    setLoading(true); // this will keep the loading state true until the data is recevied and then the state will go false
+    try {
+      const response = await login(email, password);
+      setUser(response.user);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleRegister = async (username, fullName, email, password) => {
+    setLoading(true);
+    try {
+      const response = await register(username, fullName, email, password);
+      setUser(response.user);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ user, loading, handleLogin, handleRegister }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
